b60146e1f0a5544ec116ccbeac314e7a82054a50
Sabdho Cathur Kuncoro <crhwrd@gmail.com>
2022-01-21 10:36:27 +0700

    membaca data JSON

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Arrived from './components/Arrived';
 import AsideMenu from './components/AsideMenu';
 import Browse from './components/Browse';
@@ -8,12 +8,26 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(()=> {
+    (async function() {
+      const res = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
+                              {headers: {
+                                "Content-Type": "application/json",
+                                "accept": "application/json",
+                                "x-api-key": process.env.REACT_APP_APIKEY
+                              }});
+      const {nodes} = await res.json();
+      setItems(nodes);
+    }());
+  }, []);
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <AsideMenu />
       <Footer />
